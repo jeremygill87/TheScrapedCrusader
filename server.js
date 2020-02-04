656e9d7299609f9c2b04551f0ce2c33847ac423c
Jeremy Gill <jeremygill87@gmail.com>
2020-02-04 17:00:34 -0500

    completed server.js file

--- server.js
+++ server.js
@@ -23,3 +23,32 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/articlePopulate", { useNewUrlParser: true });
+
+// Routes
+
+// A GET route for scraping the NYTimes website
+app.get("/scrape", function(req, res) {
+    // Grab the body of the html with axios
+    axios.get("http://www.nytimes.com/").then(function(response) {
+        // Load into cheerio and save it to $
+        var $ = cheerio.load(response.data);
+
+        // Empty array
+        var results = [];
+
+        $("article").each(function(i, element) {
+
+            var title = $(element).children().text();
+            var link = $(element).find("a").attr("href");
+
+            // Save results in an object that will be pushed into the results array defined earlier
+            results.push({
+                title: title,
+                link: link
+            });
+        });
+
+        console.log(results);        
+    });
+});
