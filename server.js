86a1ae2578e5a7a0128409856fc8b3519323abfa
Jeremy Gill <jeremygill87@gmail.com>
2020-03-03 08:57:53 -0500

    Connection to Heroku

--- server.js
+++ server.js
@@ -23,7 +23,10 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // Connect to the Mongo DB
-mongoose.connect("mongodb://localhost/articlePopulate", { useNewUrlParser: true });
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
 
 // Routes
 var results = [];
